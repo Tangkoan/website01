a9bfb56e5c9fb987b0c16c68a6fb965b7a2f383e
Kuy Tangkoan <kuytangkoan@gmail.com>
2026-04-05 14:53:34 +0700

    update app.php to know title tap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,7 +62,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ __('messages.app_name') ?? 'Admin Panel' }}</title>
+    <title>{{ isset($title) ? $title : __('messages.app_name') ?? 'Admin Panel' }}</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
